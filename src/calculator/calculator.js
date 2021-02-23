693c7e40611ccc86fa9fa4669e3612249b48325a
Foroutan Aghdasi <foroutan.aghdasii@gmail.com>
2021-02-23 17:26:17 +0330

    change h1 title on calculator.js file

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -1,50 +1,46 @@
-import {body, h1, keys, outputResultText, root} from "../constant/constant";
-import calculatePressBtn from "../control/calculatePressBtn";
-import calculateTypeKey from "../control/calculateTypeKey";
-import checkInputValue from "../control/checkInputValue";
+import { body, h1, keys, outputResultText, root } from "../constant/constant"
+import calculatePressBtn from "../control/calculatePressBtn"
+import calculateTypeKey from "../control/calculateTypeKey"
+import checkInputValue from "../control/checkInputValue"
 
-class Calculator{
-
-    renderPage(){
+class Calculator {
+    renderPage() {
         // crete input
-        outputResultText.setAttribute('autofocus', 'on')
-        outputResultText.setAttribute('placeholder', '0')
-        outputResultText.setAttribute('type', 'text')
-        outputResultText.classList.add('input')
+        outputResultText.setAttribute("autofocus", "on")
+        outputResultText.setAttribute("placeholder", "0")
+        outputResultText.setAttribute("type", "text")
+        outputResultText.classList.add("input")
         root.appendChild(outputResultText)
         //create text at top of body
-        h1.innerText = `Calculator Project - Foroutan Aghdasi`
+        h1.innerText = `Calculator Project `
         body.appendChild(h1)
-        body.insertBefore(h1,body.childNodes[0])
+        body.insertBefore(h1, body.childNodes[0])
 
         // load all buttons and operators
         for (let i = 0; i < keys.length; i++) {
-
-            let div = document.createElement('div')
-            div.classList.add('row')
+            let div = document.createElement("div")
+            div.classList.add("row")
 
             for (let y = 0; y < keys[i].length; y++) {
-                let btn = document.createElement('div')
+                let btn = document.createElement("div")
                 btn.innerText = keys[i][y]
-                btn.classList.add('btn')
-                btn.addEventListener('click', calculatePressBtn)
+                btn.classList.add("btn")
+                btn.addEventListener("click", calculatePressBtn)
 
-                outputResultText.addEventListener('keyup', calculateTypeKey)
-                outputResultText.addEventListener('keypress', checkInputValue)
+                outputResultText.addEventListener("keyup", calculateTypeKey)
+                outputResultText.addEventListener("keypress", checkInputValue)
                 div.appendChild(btn)
-
             }
             root.appendChild(div)
 
             // width 100% for equal and CE
-            let equal = document.getElementsByClassName('btn');
+            let equal = document.getElementsByClassName("btn")
 
             for (let el of equal) {
-                if (el.innerText === '=') {
-                    el.style.width = '93%'
+                if (el.innerText === "=") {
+                    el.style.width = "93%"
                 }
             }
-
         }
     }
 }
